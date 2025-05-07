17f1af289b37f31f5aed6adcbfe04ef98be2df5e
IvanSensa <ivan.thrastarson@sensa.is>
2025-05-07 21:09:08 +0000

    Fix grid layout

--- apps/sensa-frontend/components/News/News.css.ts
+++ apps/sensa-frontend/components/News/News.css.ts
@@ -3,37 +3,45 @@ import { style } from '@vanilla-extract/css'
 
 export const root = style({
   display: 'flex',
-  flexDirection: 'row',
-  width: '100%',
-  boxSizing: 'border-box',
-  gap: '16px', // optional: spacing between boxes
-
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'flex-start',
+  gap: '16px',
+  alignSelf: 'stretch',
 })
 
+export const header = style({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  gap: '8px',
+  alignSelf: 'stretch',
+})
+
+
+export const content = style({
+  display: "flex",
+alignContent: "flex-start",
+gap: "24px",
+alignSelf: "stretch",
+flexWrap: "wrap",
+alignItems: "stretch",
+
+})
 
 export const box = style({
-  paddingTop: '24px',
-  paddingRight: '32px',
-  paddingBottom: '24px',
-  paddingLeft: '32px',
-  display: 'flex',
-  flexDirection: 'column',
-  width: '50%',
-  borderRadius: '8px',
-  border: '1px solid #CCDFFF'
+  justifyContent: 'space-between',
+  flex: '1 1 0',
+  minWidth: '600px',
+  padding: '40px',
 })
 
-export const box1 = style({
-  paddingTop: '24px',
-  paddingRight: '32px',
-  paddingBottom: '24px',
-  paddingLeft: '32px',
-  display: 'flex',
-  flexDirection: 'column',
-  width: '50%',
-  borderRadius: '8px',
-})
 
-export const player = style({
-  marginBottom: '10px'
+export const innerNewsCard = style({
+  display: "flex",
+flexDirection: "column",
+alignItems: "flex-start",
+gap: "16px",
+flex: "1 0 0",
+alignSelf: "stretch",
 })
