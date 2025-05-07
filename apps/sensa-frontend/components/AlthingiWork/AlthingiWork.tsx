35cbee2b119ca2a2ca20fc685789655f96412536
IvanSensa <ivan.thrastarson@sensa.is>
2025-05-07 23:26:54 +0000

    Recorded Sessions link

--- apps/sensa-frontend/components/AlthingiWork/AlthingiWork.tsx
+++ apps/sensa-frontend/components/AlthingiWork/AlthingiWork.tsx
@@ -15,7 +15,7 @@ const AlthingiWork = () => {
         </Text>
         <Box className={styles.cardContainer}>
           <CategoryCard href='/work/peopleAndCommittees' heading='People and Committees' text='Explore the structure of Parliament through profiles of Members of Parliament, political party affiliations, parliamentary committees, and the Speaker’s Committee.' />
-          <CategoryCard heading='Sessions' text='Access recordings, agendas, transcripts, and search tools related to parliamentary sessions and speeches, including speaking schedules, speech archives, and rules on speaking time.' />
+          <CategoryCard href='sessions/recordedSessions' heading='Sessions' text='Access recordings, agendas, transcripts, and search tools related to parliamentary sessions and speeches, including speaking schedules, speech archives, and rules on speaking time.' />
           <CategoryCard href='/work/sessionsAndBills' heading='Laws, Resolutions and Bills' text='Browse Iceland’s legislative work, including collections of laws, resolutions, and bills, as well as inquiries, reports, opinions, and the current status of parliamentary matters.' />
         </Box>
       </Box>
