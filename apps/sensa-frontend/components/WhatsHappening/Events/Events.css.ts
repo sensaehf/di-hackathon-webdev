4773eab2158b18b13450291f8fe15bf8fbd52ee2
IvanSensa <ivan.thrastarson@sensa.is>
2025-05-07 21:32:53 +0000

    Main Page UI

--- apps/sensa-frontend/components/WhatsHappening/Events/Events.css.ts
+++ apps/sensa-frontend/components/WhatsHappening/Events/Events.css.ts
@@ -1,7 +1,41 @@
-import { style } from "@vanilla-extract/css";
+import { style } from '@vanilla-extract/css'
 
 export const root = style({
   backgroundColor: '#F8F5FA',
-  height: '100%',
+  display: 'flex',
+  padding: '24px',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+  alignItems: 'flex-start',
+  flex: '1 0 0',
+  alignSelf: 'stretch',
+})
 
+export const innerBox = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  gap: '16px',
+  alignSelf: 'stretch',
+})
+
+export const day = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  gap: '8px',
+  alignSelf: 'stretch',
+})
+
+export const timeBox = style({
+  width: "96px",
+minWidth: "80px",
+})
+
+export const line = style({
+  display: 'flex',
+  justifyContent: 'flex-start',
+  alignItems: 'center',
+  gap: '8px',
+  alignSelf: 'stretch',
 })
--- apps/sensa-frontend/components/WhatsHappening/Events/Events.tsx
+++ apps/sensa-frontend/components/WhatsHappening/Events/Events.tsx
@@ -1,15 +1,52 @@
-import { Box, Text } from '@island.is/island-ui/core'
+import { Box, Button, Text } from '@island.is/island-ui/core'
 import * as styles from './Events.css'
 const Events = () => {
   return (
     <Box className={styles.root}>
-      <Box>
-        {/* Inner box */}
-        <Box>{/* Monday */}
-          <Text>Monday</Text>
+      <Box className={styles.innerBox}>
+        <Box className={styles.day}>
+          <Text variant="eyebrow">Monday may 5</Text>
+          <Box className={styles.line}>
+            <Box className={styles.timeBox}>
+              <Text>3.00 PM</Text>
+            </Box>
+            <Button variant="text">Parliamentary session</Button>
+          </Box>
         </Box>
-        <Box></Box> {/* Inner box */}
+
+        <Box className={styles.day}>
+          <Text variant="eyebrow">Monday may 5</Text>
+          <Box className={styles.line}>
+            <Box className={styles.timeBox}>
+              <Text>3.00 PM</Text>
+            </Box>
+            <Button variant="text">Parliamentary session</Button>
+          </Box>
+          <Box className={styles.line}>
+            <Box className={styles.timeBox}>
+              <Text>3.00 PM</Text>
+            </Box>
+            <Button variant="text">Parliamentary session</Button>
+          </Box>
+          <Box className={styles.line}>
+            <Box className={styles.timeBox}>
+              <Text>3.00 PM</Text>
+            </Box>
+            <Button variant="text">Parliamentary session</Button>
+          </Box>
+
+          <Box className={styles.line}>
+            <Box className={styles.timeBox}>
+              <Text>3.00 PM</Text>
+            </Box>
+            <Button variant="text">Parliamentary session</Button>
+          </Box>
+        </Box>
       </Box>
+
+      <Button variant="text" size="small">
+        View all planned events
+      </Button>
     </Box>
   )
 }
