289789e3e6639dd72b450a0df929d579cb042557
IvanSensa <ivan.thrastarson@sensa.is>
2025-05-08 01:49:56 +0000

    Sessions and bills translated

--- apps/sensa-frontend/screens/LawsScreen/LawsScreen.tsx
+++ apps/sensa-frontend/screens/LawsScreen/LawsScreen.tsx
@@ -2,20 +2,41 @@ import { Box, Divider, Text } from '@island.is/island-ui/core'
 import Card from './components/Card/Card'
 import { Webreader } from '../../components/Webreader'
 import * as styles from './LawsScreen.css'
+import { useI18n } from '../../i18n'
+
 const LawsScreen = () => {
-  // TODO i18n
+  const i18n = useI18n()
+  const { laws: lawsSection, resolutions, bills } = i18n.t.laws.sections
+
   return (
     <>
       <Text variant="h2" as="h2">
-        Laws, Resolutions and Bills
+        {i18n.t.laws.title}
       </Text>
       <Webreader />
+
       <Box className={styles.cardContainer}>
-        <Card className={styles.card} title="Laws" shortcuts={[]} />
+        <Card
+          className={styles.card}
+          title={lawsSection.title}
+          description={lawsSection.description}
+          shortcuts={lawsSection.shortcuts}
+        />
         <Divider />
-        <Card className={styles.card} title="Resolutions" shortcuts={[]} />
+        <Card
+          className={styles.card}
+          title={resolutions.title}
+          description={resolutions.description}
+          shortcuts={resolutions.shortcuts}
+        />
         <Divider />
-        <Card className={styles.card} title="Bills" shortcuts={[]} href={'/work/lawsResolutionsAndBills/bills'} />
+        <Card
+          className={styles.card}
+          title={bills.title}
+          description={bills.description}
+          shortcuts={bills.shortcuts}
+          href="/work/lawsResolutionsAndBills/bills"
+        />
       </Box>
     </>
   )
