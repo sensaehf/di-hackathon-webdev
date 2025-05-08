3a48bfcfaff26d21f2d4b9f7e37fec7ba9a2f80f
IvanSensa <ivan.thrastarson@sensa.is>
2025-05-08 02:07:19 +0000

    Members of parliament tranlsate

--- apps/sensa-frontend/components/MockSearch/MockSearch.tsx
+++ apps/sensa-frontend/components/MockSearch/MockSearch.tsx
@@ -1,14 +1,14 @@
 import { AsyncSearch } from '@island.is/island-ui/core'
-import { getAllParliamentMembersAsSearchItem } from 'apps/sensa-frontend/data/endpoints';
-import { useI18n } from 'apps/sensa-frontend/i18n';
-import { useRouter } from 'next/router';
+import { getAllParliamentMembersAsSearchItem } from '../../data/endpoints'
+import { useI18n } from '../../i18n'
+import { useRouter } from 'next/router'
 import { useState } from 'react'
 
 const MockSearch = () => {
   const i18n = useI18n()
-  const router = useRouter();
-  const pmOptions = getAllParliamentMembersAsSearchItem();
-  
+  const router = useRouter()
+  const pmOptions = getAllParliamentMembersAsSearchItem()
+
   const [value, setValue] = useState('')
   const [selected, setSelected] = useState<null | typeof pmOptions[0]>(null)
 
@@ -16,25 +16,23 @@ const MockSearch = () => {
     setValue(inputValue)
     setSelected(selectedOption)
 
-    console.log('selectedOption', selectedOption);
-
-    if(selectedOption !== null){
-      router.push(`${i18n.t.routes.member}/${selectedOption.value}`); 
+    if (selectedOption !== null) {
+      router.push(`${i18n.t.routes.member}/${selectedOption.value}`)
     }
   }
 
   return (
-    <>
-      <AsyncSearch
-        placeholder="Search Alþingi"
-        options={pmOptions}
-        inputValue={value}
-        onSubmit={handleSubmit}
-        onChange={(selectedItem) => setSelected(selectedItem)}
-        showDividerIfActive
-        size="medium"
-      />
-    </>
+    <AsyncSearch
+      placeholder={i18n.t.search.placeholder}
+      options={pmOptions}
+      inputValue={value}
+      onSubmit={handleSubmit}
+      onChange={(selectedItem) => setSelected(selectedItem)}
+      noOptionsMessage={() => i18n.t.search.noResults}
+      loadingMessage={() => i18n.t.search.loading}
+      showDividerIfActive
+      size="medium"
+    />
   )
 }
 
